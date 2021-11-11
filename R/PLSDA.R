2769ce82e282f5f3c3fbfb503022f1a9e92b8625
Manon Martin <manon.martin@uclouvain.be>
2021-11-11 10:02:01 +0100

    added validation PLSDA

--- R/PLSDA.R
+++ R/PLSDA.R
@@ -8,6 +8,8 @@
 #' @param x A numeric matrix of spectral intensities.
 #' @param y The real groups' memberships of the samples.
 #' @param nLV The number of latent variables for PLS-DA.
+#' @param validation The type of validation (see `pls::plsr()` for more info).
+#' @param ... Additional arguments to be passed to `pls::plsr()` (eg: `segments`).
 #'
 #' @param drawRMSEP Logical, if \code{'TRUE'}, will draw plot of RMSEP values to decide on the number of latent variables.
 #' @return A list with the following elements:
@@ -24,25 +26,31 @@
 #'   \item{\code{coefficients}}{PLSDA coefficients}
 #'   \item{\code{scores}}{PLSDA scores}
 #'   \item{\code{loadings}}{PLSDA loadings}
+#'   \item{\code{VIP}}{The variable VIPs}
 #'    }
 #' @author Manon Martin
 #'
 #' @examples
 #' data('HumanSerum')
-#' PLSDA = function(x = HumanSerumSpectra, y = ClassHS, nLV = NULL, drawRMSEP = TRUE)
+#' 
+#' PLSDA = PLSDA(x = HumanSerumSpectra,  y = ClassHS, nLV = NULL, drawRMSEP = TRUE, validation = "LOO")
+#' 
+#' PLSDA = PLSDA(x = HumanSerumSpectra,  y = ClassHS, nLV = NULL, drawRMSEP = TRUE, validation = "CV", segments = 5)
 #'
 #'@importFrom pls cppls mvrValstats RMSEP R2 MSEP
 #'@importFrom stats model.matrix
 #'@importFrom graphics axis mtext par plot
+#'@importFrom plsVarSel VIP
 
 
 
-PLSDA <- function(x, y, nLV = NULL, drawRMSEP = TRUE) {
+PLSDA <- function(x, y, nLV = NULL, drawRMSEP = TRUE, 
+                  validation = c("CV", "none", "LOO"), ...) {
 
 
   checkArg(nLV, "num", can.be.null = TRUE)
   checkArg(drawRMSEP, "bool", can.be.null = FALSE)
-
+  validation <- match.arg(validation)
 
   ######################### I. Initialisations #
 
@@ -69,6 +77,9 @@ PLSDA <- function(x, y, nLV = NULL, drawRMSEP = TRUE) {
   dummy
   ytrain.mat <- matrix(dummy, nrow = length(y))
 
+  if (nlevels(factor(ytrain))==2){
+    ytrain.mat <- ytrain.mat[,1,drop=FALSE]
+  }
 
   ################################################# II.PLS-DA #
 
@@ -78,13 +89,21 @@ PLSDA <- function(x, y, nLV = NULL, drawRMSEP = TRUE) {
 
   # ------------------- Cross-validation ------------------- -------------------
 
-  pls1 <- pls::plsr(ytrain.mat ~ xtrain, scale = FALSE, validation = "CV", model = TRUE)
+  pls1 <- pls::plsr(ytrain.mat ~ xtrain, scale = FALSE, validation = validation,
+                    model = TRUE, ...)
 
   # ncomp = min(5, pls1$ncomp) pls::mvrValstats(pls1, estimate='train')
 
   # % of variance explained ------------------------
   explvarYcum <- 100 * drop(pls::R2(pls1, estimate = "train", intercept = FALSE)$val)
+  
+  if (nlevels(factor(ytrain))==2){
+    explvarYcum <- t(as.matrix(explvarYcum))
+    rownames(explvarYcum) <- "Y1"
+  } 
   explvarY <- explvarYcum
+  
+  
   for (i in 2:dim(explvarY)[2]) {
     explvarY[, i] <- explvarYcum[, i] - explvarYcum[, (i - 1)]
   }
@@ -98,11 +117,29 @@ PLSDA <- function(x, y, nLV = NULL, drawRMSEP = TRUE) {
 
 
   if (drawRMSEP == TRUE) {
+    
+    nvals <- min(20,pls1$ncomp)
+    
     # R^2: coefficient of multiple determination
-    R2 <- drop(pls::R2(pls1, estimate = "train", intercept = FALSE)$val)  #'train': training or calibration data estimate
-
+    Q2 <- drop(pls::R2(pls1, estimate = "CV", intercept = FALSE)$val) 
+    if (nlevels(factor(ytrain))==2){
+      Q2 <- t(as.matrix(Q2))
+      rownames(Q2) <- "Y1"
+    } 
+    
+    Q2 <- Q2[,1:nvals, drop=FALSE]
+    Q2 <- t(Q2)
+    
     # RMSEP
-    RMSEP <- drop(pls::RMSEP(pls1, intercept = FALSE)$val)
+    RMSEP <- drop(pls::RMSEP(pls1, intercept = FALSE, estimate = "CV")$val)
+    if (nlevels(factor(ytrain))==2){
+      RMSEP <- t(as.matrix(RMSEP))
+      rownames(RMSEP) <- "Y1"
+    } 
+    
+    RMSEP <- RMSEP[,1:nvals, drop=FALSE]
+    RMSEP <- t(RMSEP)
+    
     if (dim(RMSEP)[2] < 3) {
       par(mfrow = c(1, dim(RMSEP)[2]), oma = c(0, 0, 4, 0), mar = c(3, 4, 2,
         4) + 0.1)
@@ -112,12 +149,14 @@ PLSDA <- function(x, y, nLV = NULL, drawRMSEP = TRUE) {
     }
 
     for (i in 1:dim(RMSEP)[2]) {
-      plot(R2[i, 1:10], type = "l", ylab = "R2", xlab = "", main = paste0("Y",i))
-
+      plot(1:nvals, Q2[1:nvals,i], type = "b", ylab = "Q2", xlab = "", main = paste0("Y",i))
+      abline(v=nLV)
+      
       par(new = TRUE)
-      plot(RMSEP[1, i, 1:10], type = "l", lty = 2, col = "red", axes = FALSE,
+      plot(1:nvals, RMSEP[1:nvals,i], type = "b", lty = 2, col = "red", axes = FALSE,
             xlab = "", ylab = "")
-      mtext("RMSEP", side = 4, col = "red", line = 3)
+      abline(v=nLV)
+      mtext("RMSECV", side = 4, col = "red", line = 3)
       axis(4, col = "red", col.axis = "red", las = 1)
 
     }
@@ -131,7 +170,8 @@ PLSDA <- function(x, y, nLV = NULL, drawRMSEP = TRUE) {
   # -----------------------------------------------------
 
   # Estimation du modele final
-  pls <- pls::plsr(ytrain.mat ~ xtrain, ncomp = nLV, scale = FALSE, validation = "CV")
+  pls <- pls::plsr(ytrain.mat ~ xtrain, ncomp = nLV, scale = FALSE, 
+                   validation = validation, ...)
 
 
   Xscores <- cbind(pls$scores)
@@ -140,28 +180,35 @@ PLSDA <- function(x, y, nLV = NULL, drawRMSEP = TRUE) {
   R2 <- pls::R2(pls, estimate = "train", intercept = FALSE)$val[1, , nLV]  #'train': training or calibration data estimate
 
   # RMSEP
-  RMSEP <- pls::RMSEP(pls, intercept = FALSE)$val[1, , nLV]
+  RMSEP <- pls::RMSEP(pls, estimate = "CV", intercept = FALSE)$val[1, , nLV]
 
   # Q2 : cross-validated R^2
 
   ## cross-validated R^2:
   Q2 <- pls::R2(pls, estimate = "CV", intercept = FALSE)$val[1, , nLV]
 
-  # ===== PRESS
-  PRESS <- apply(pls$validation$PRESS, 2, sum)
+  
 
   # PRESS = apply(pls::mvrValstats(pls, estimate='CV')$SSE[1,,],2,sum) # same PRESS
   # values
 
   # SSE
   pls::mvrValstats(pls, estimate = "train")$SSE[1, , ]  #same residual sum of squares: residuals = pls$residuals[,,(nLV-1)]^2
-  SS <- apply(pls::mvrValstats(pls, estimate = "train")$SSE[1, , ], 2, sum)
-  SS <- SS[-nLV - 1]
+  
+  if (nlevels(factor(ytrain))==2){
+    Q2cum <- Q2
+  } else{ 
+    # ===== PRESS
+    PRESS <- apply(pls$validation$PRESS, 2, sum)
+    
+    SS <- apply(pls::mvrValstats(pls, estimate = "train")$SSE[1, , ], 2, sum) 
+    SS <- SS[-nLV - 1]
+    Q2cum <- 1 - prod(PRESS/SS)
+  }
 
+  
 
-  Q2cum <- 1 - prod(PRESS/SS)
-
-
+  VIP <- plsVarSel::VIP(pls, opt.comp = nLV)
 
   # Preparation des sorties ----------------------------------------
   # ----------------------------------------
@@ -169,7 +216,8 @@ PLSDA <- function(x, y, nLV = NULL, drawRMSEP = TRUE) {
   #
   rpls <- list(original.dataset = xtrain, Y = ytrain.mat, nLV = nLV, RMSEP = RMSEP,
               R2 = R2, Q2 = Q2, Q2cum = Q2cum, ExpVarY = ExpVarY, ExpVarX = ExpVarX,
-              coefficients = pls$coefficients, scores = pls$scores, loadings = pls$loadings)
+              coefficients = pls$coefficients, scores = pls$scores, loadings = pls$loadings,
+              VIP = VIP)
 
   return(rpls)
 }
